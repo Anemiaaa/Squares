b29c9264332feecd7e3dcd2dca371289c4d121d0
Anemiaaa <diangeloq@gmail.com>
2022-01-10 17:11:45 +0200

    LandingView - animation problems

---
+++ Squares/Squares/F.swift
@@ -0,0 +1,13 @@
+//
+//  F.swift
+//  Squares
+//
+//  Created by mac on 10.01.2022.
+//
+
+import Foundation
+
+public enum F {
+   
+    typealias VoidFunc = () -> ()
+}
--- Squares/Squares/UI/LandingView.swift
+++ Squares/Squares/UI/LandingView.swift
@@ -8,11 +8,6 @@
 import UIKit
 import RxSwift
 
-public enum F {
-   
-    typealias VoidFunc = () -> ()
-}
-
 public enum States {
     
     case nextClick
@@ -23,7 +18,7 @@ class LandingView: UIView {
     // MARK: -
     // MARK: Variables
     
-    @IBOutlet weak var square: UIView?
+    @IBOutlet weak var figure: UIView?
     @IBOutlet weak var nextButton: UIButton?
     
     public var viewStatesHandler = PublishSubject<States>()
@@ -31,24 +26,50 @@ class LandingView: UIView {
     // MARK: -
     // MARK: Public
     
-    public func moveSquare(to position: Positions, animated: Bool, completion: F.VoidFunc?) {
+    public func moveFigure(to position: Positions, animated: Bool, completion: F.VoidFunc?) {
+        let startCorner: CGFloat
+        let endCorner: CGFloat
+        let endColor: UIColor
         let point = self.origin(from: position)
         
+        if position == .leftUp || position == .rightUp {
+            startCorner = 0
+            endCorner = (self.figure?.bounds.size.width ?? 0) / 2
+            endColor = position == .leftUp ? .systemTeal : .cyan
+        }
+        else {
+            startCorner = (self.figure?.bounds.size.width ?? 0) / 2
+            endCorner = 0
+            endColor = position == .rightBottom ? .green : .blue
+        }
+        
+        
         if animated {
-            LandingView.animate(
-                withDuration: 1,
-                animations: { self.square?.frame.origin = point },
-                completion: { _ in completion?() }
-            )
+            
+            CATransaction.begin()
+            
+            CATransaction.setAnimationDuration(2)
+            CATransaction.setCompletionBlock {
+                completion?()
+            }
+
+            self.switchSquareCircle(from: startCorner, to: endCorner, duration: 2)
+            self.changeColor(to: endColor, duration: 2)
+            self.move(to: point, duration: 2)
+            
+            CATransaction.commit()
+            
         } else {
-            self.square?.frame.origin = point
+            self.figure?.frame.origin = point
+            self.figure?.layer.cornerRadius = endCorner
+            
             completion?()
         }
     }
     
     public func origin(from position: Positions) -> CGPoint {
         let bound = self.bounds
-        let size = self.square?.bounds.size ?? .zero
+        let size = self.figure?.bounds.size ?? .zero
         
         switch position {
         case .leftUp:
@@ -67,5 +88,35 @@ class LandingView: UIView {
     
     @IBAction private func onClickNextButton(_ sender: Any) {
         self.viewStatesHandler.onNext(.nextClick)
+    }
+    
+    private func switchSquareCircle(from start: CGFloat, to end: CGFloat, duration: CFTimeInterval) {
+        let animation = CABasicAnimation(keyPath: "circle")
+        
+        animation.fromValue = start
+        animation.toValue = end
+        animation.duration = duration
+        
+        self.figure?.layer.add(animation, forKey: "circle")
+    }
+    
+    private func changeColor(to color: UIColor, duration: CFTimeInterval) {
+        let animation = CABasicAnimation(keyPath: "color")
+        
+        animation.fromValue = self.figure?.backgroundColor
+        animation.toValue = color
+        animation.duration = duration
+        
+        self.figure?.layer.add(animation, forKey: "color")
+    }
+    
+    private func move(to point: CGPoint, duration: CFTimeInterval) {
+        let animation = CABasicAnimation(keyPath: "movement")
+        
+        animation.fromValue = self.figure?.frame.origin
+        animation.toValue = point
+        animation.duration = duration
+        
+        self.figure?.layer.add(animation, forKey: "movement")
     }
 }
